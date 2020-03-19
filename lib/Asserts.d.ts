c58e8bcde77551ac6abe84dd0d16d4487686fdd0
Anatolii Yakushko <shaddyx@gmail.com>
2020-03-19 20:34:41 +0200

    [-] fixed static

--- lib/Asserts.d.ts
+++ lib/Asserts.d.ts
@@ -1,8 +1,12 @@
 declare type ErrFunc = (() => string | Error) | string | Error;
-export declare class Asserts {
-    private errorOrString;
-    private assertionMsg;
-    assertTrue(val: any, msg?: ErrFunc): void;
+export default class Asserts {
+    private static errorOrString;
+    private static assertionMsg;
+    static assertTrue(val: any, msg?: ErrFunc): void;
+    static assertFalse(val: any, msg?: ErrFunc): void;
+    static assertDefined(val: any, msg?: ErrFunc): void;
+    static assertEquals(val: any, val1: any, msg?: ErrFunc): void;
+    static assertNotEquals(val: any, val1: any, msg?: ErrFunc): void;
 }
 export {};
 //# sourceMappingURL=Asserts.d.ts.map
--- src/Asserts.ts
+++ src/Asserts.ts
@@ -1,38 +1,38 @@
 type ErrFunc = (() => string | Error ) | string | Error;
-export class Asserts {
-    private errorOrString(m: string | Error): Error{
+export default class Asserts {
+    private static errorOrString(m: string | Error): Error{
         if (typeof m === "string"){
             return new Error(m);
         } else {
             return m;
         }
     }
-    private assertionMsg(msg:ErrFunc): Error{
+    private static assertionMsg(msg:ErrFunc): Error{
         return this.errorOrString(typeof msg == "function" ? msg(): msg);
     }
 
-    public assertTrue(val: any, msg:ErrFunc = "Value should be true"){
+    public static assertTrue(val: any, msg:ErrFunc = "Value should be true"){
         if (!val){
             throw this.assertionMsg(msg);
         }
     }
 
-    public assertFalse(val: any, msg:ErrFunc = "Value should be false"){
+    public static assertFalse(val: any, msg:ErrFunc = "Value should be false"){
         if (val){
             throw this.assertionMsg(msg);
         }
     }
-    public assertDefined(val: any, msg:ErrFunc = "Value should be defined"){
+    public static assertDefined(val: any, msg:ErrFunc = "Value should be defined"){
         if (val != undefined){
             throw this.assertionMsg(msg);
         }
     }
-    public assertEquals(val: any, val1: any, msg:ErrFunc = "Values should be equals"){
+    public static assertEquals(val: any, val1: any, msg:ErrFunc = "Values should be equals"){
         if (val !== val1){
             throw this.assertionMsg(msg);
         }
     }
-    public assertNotEquals(val: any, val1: any, msg:ErrFunc = "Value should be not equals"){
+    public static assertNotEquals(val: any, val1: any, msg:ErrFunc = "Value should be not equals"){
         if (val === val1){
             throw this.assertionMsg(msg);
         }
